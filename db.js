0d41cf708e94b2e6c2ba033c9c3d1395f379e6aa
Muhammad Amir <umairamir1127@gmail.com>
2024-07-19 22:10:03 -0400

    database

---
+++ db.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+// insert your database connection string here
+mongoose.connect('')
+
+.then(()=> {
+    console.log('connected to the database')
+})
+.catch(() => {
+    console.log('connection failed')
+})
+
+const loginSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    email: {
+        type: String,
+        required: true
+    }
+})
+
+const loginCollection = new mongoose.model('loginCollection',loginSchema)
+
+module.exports = loginCollection
